16c9e065b1592ebeed9bd8fac56e2196c4d298ef
asoolsadaqa <asoolsadaqa2006@gmail.com>
2025-12-25 18:10:42 +0300

    Add files via upload

---
+++ Citadel_walker.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import random
+
+def create_street_contour():
+    t = list(np.arange(0, 361,10))
+    x = []
+    for i in t:
+        x.append((33 * np.cos(i*(math.pi/180))))
+
+    y = []
+    for i in t:
+        y.append((33 * np.sin(i*(math.pi/180)))+random.randint(-10,10))
+
+    return x,y
+
+x,y=create_street_contour()
+plt.plot(x,y)
+plt.show()
